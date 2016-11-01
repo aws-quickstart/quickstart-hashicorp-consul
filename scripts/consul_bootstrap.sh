68ac42fb9b4a6102aa0242566a0f8e5d3eec2467
Anthony Vattathil <avattathil@gmail.com>
2016-11-01 13:57:17 -0700

    Added check_bootstrap script

--- scripts/consul_bootstrap.sh
+++ scripts/consul_bootstrap.sh
@@ -184,9 +184,22 @@ chmod 0755 /usr/local/bin/consul-template
 chown root:root /usr/local/bin/consul-template
 chkstatus
 
-wget https://s3.amazonaws.com/quickstart-reference/hashicorp/consul/latest/scripts/check_bootstrap.sh
+echo "Starting Node Scanner in background! (see /tmp/check.log)"
+curl -s ${S3SCRIPT_PATH}/check_bootstrap.sh -o /tmp/check.sh
+sed -i "s/__CONSUL_EXPECT__/${CONSUL_EXPECT}/" /tmp/check.sh
+chmod 755 /tmp/check.sh
+/bin/bash -c '/tmp/check.sh' 
 chkstatus
-chmod 755 check_bootstrap.sh
-export RUN="./check_bootstrap.sh ${CONSUL_EXPECT} &>/tmp/check_bootstrap.log"
-/bin/bash -c '${RUN}' &
+
+echo Installing Dnsmasq...
+sudo apt-get -qq -y update
+sudo apt-get -qq -y install dnsmasq-base dnsmasq
+
+echo Configuring Dnsmasq...
+sudo sh -c 'echo "server=/consul/127.0.0.1#8600" >> /etc/dnsmasq.d/consul'
+sudo sh -c 'echo "listen-address=127.0.0.1" >> /etc/dnsmasq.d/consul'
+sudo sh -c 'echo "bind-interfaces" >> /etc/dnsmasq.d/consul'
+
+echo "Restarting dnsmasq..."
+sudo service dnsmasq restart
 chkstatus
